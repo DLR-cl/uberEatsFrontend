0efd7a1de309782e5ec000d7d91751462c267df2
Juan Yampara <juan.yampara.rojas@gmail.com>
2024-04-22 19:39:09 -0400

    definiendo directorios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CurtainComponent } from './container/curtain/curtain/curtain.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'index', component: CurtainComponent},
+];
