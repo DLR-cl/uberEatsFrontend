409d16d35a2aed88e5843684fda8d551aa16e8d7
crissspi <cristinatiticortez@gmail.com>
2024-04-24 00:04:53 -0400

    footer creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { FooterHomeComponent } from './pages/footer-home/footer-home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent],
+  imports: [RouterOutlet, FooterHomeComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
