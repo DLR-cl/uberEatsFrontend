88918fcf94af3c51f825de46008f25fc5961d99a
Juan Yampara <juan.yampara.rojas@gmail.com>
2024-04-26 18:50:53 -0400

    Avances pag producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductoComponent } from './pages/producto/producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'producto', component: ProductoComponent},
+];
