7f5d74137226bc500797f11e1fb47a2259899a4f
crissspi <cristinatiticortez@gmail.com>
2024-04-27 19:52:38 -0400

    desarrollo pagina carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LocalComponent } from './pages/local/local.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'local', component: LocalComponent},
+    {path: 'carrito', component: CarritoComponent}
 ];
