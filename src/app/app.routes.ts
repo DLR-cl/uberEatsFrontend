5ae50c325a79f516d742bc27637199dc201fd07f
crissspi <cristinatiticortez@gmail.com>
2024-04-27 13:57:04 -0400

    desarrollo página local

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { LocalComponent } from './pages/local/local.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
+    {path: 'local', component: LocalComponent},
 ];
