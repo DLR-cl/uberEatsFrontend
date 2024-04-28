da6f3a1b3c798fca847fa38be4564540c6ed565d
crissspi <cristinatiticortez@gmail.com>
2024-04-27 22:06:15 -0400

    arreglo footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { LocalComponent } from './pages/local/local.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
 import { ProductoComponent } from './pages/producto/producto.component';
+import { FooterHomeComponent } from './pages/footer-home/footer-home.component';
 
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'local', component: LocalComponent},
     {path: 'producto', component: ProductoComponent},
-    {path: 'carrito', component: CarritoComponent}
-  
-
+    {path: 'carrito', component: CarritoComponent},
+    {path: 'footer', component: FooterHomeComponent}
 ];
